2b14ac510c43e2d009f172013cbe6f6155cc4254
shaunliaw122 <128031843+shaunliaw122@users.noreply.github.com>
2024-10-14 15:15:14 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,32 @@
+var vg_1 = "map.vg.json";
+var vg_2 = "co2trend.vg.json"
+var vg_3 = "cumulativeco2.vg.json"
+var vg_4 = "diffemisiontype.vg.json"
+var vg_5 = "gspvsco2.vg.json"
+var vg_6 =  "butterfly.vg.json"
+
+
+
+vegaEmbed("#map1", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#map2", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#map3", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#map4", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#map5", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#map6", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
